926112c582ba0dc7775b7722b6297154bf260b28
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-04-06 14:04:49 -0400

    feat:brabded updated

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
     template: `<div class="layout-footer px-4 py-3 text-center">
         <div class="text-xs sm:text-sm md:text-base">
             Built with precision by
-            <a routerLink="/auth/login" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline mx-1">TradexCoin</a>
+            <a routerLink="/auth/login" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline mx-1">SignalSwaps</a>
             — Smart AI Trading Platform
         </div>
     </div>`
